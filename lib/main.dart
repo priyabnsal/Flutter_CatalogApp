3a68e02051b85c22a2b7f176f235dbe77bd9047d
Priya Bansal <priyabansal.cse@gmail.com>
2021-07-27 02:39:01 +0530

    Login Page design

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:first/pages/homepage.dart';
 import 'package:first/pages/login.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main(){
   runApp(MyApp());
 }
@@ -13,14 +14,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: HomePage(),
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple
-      ),
-      themeMode: ThemeMode.light,
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+            fontFamily:GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      initialRoute: "/LoginPage",
       routes: {
         "/": (context) => LoginPage(),
         "/home" : (context) => HomePage(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,14 +5,47 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login",style: TextStyle(
-          // fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ),
-          textScaleFactor: 3.0,
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/img/login1.png",
+            fit: BoxFit.cover ,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+          child:Column(children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter Unername",
+                labelText: "Username",
+              ),
+            ),
+            TextFormField(
+              obscureText:true,
+              decoration: InputDecoration(
+                hintText: "Enter Password",
+                labelText: "Password",
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(onPressed: () {print("Hi Priya");}, child: Text("Login"))
+          ],),),
+
+        ],
       ),
     );
   }
